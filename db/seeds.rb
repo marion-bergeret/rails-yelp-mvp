4a011716719e8c4a77729f9a77cad7934eb18216
Marion Bergeret <bergeret.marion@gmail.com>
2018-03-07 12:31:38 +0100

    exercise completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese',
+    phone_number: '01 45 56 66 77',
+
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number: '01 45 56 66 77',
+
+  },
+  {
+    name:         'Pizza East2',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number: '01 45 56 66 77',
+
+  },
+  {
+    name:         'Pizza East3',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number: '01 45 56 66 77',
+
+  },
+  {
+    name:         'Pizza East4',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number: '01 45 56 66 77',
+
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
